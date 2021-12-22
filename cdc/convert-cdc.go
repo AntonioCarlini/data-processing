838bd9f3adad96adbcd24526fe13b0c3c8b9d1d8
Antonio Carlini <antonio@acarlini.com>
2021-12-22 22:01:00 +0000

    Cope with a more complicated set of transactions from crypto.com
    The code now explicitly handles more transaction types and in fact
    handles all the ones that have been seen so far.
    
    Transactions are now grouped (in forward time order) by cryptocurency,
    to ease updating any spreadsheet in the echange-independent format.

--- cdc/convert-cdc.go
+++ cdc/convert-cdc.go
@@ -4,6 +4,8 @@ package main
 // Where applicable it rearranges each row into a format that matches my exchange-independent records.
 // Each row entry that is expected to have a meaningful value is checked and errors are flagged on the command line.
 // Some rows do not produce any output but as much checking as possible is performed anyway to try to avoid silent corruption.
+//
+// Output is grouped by cryptocurrency and presented in ascending date order within each group.
 
 // Usage:
 // The program takes two parameters: the input transactions (in CSV format) and a file into which to write the resulting converted transactions (in CSV format).
@@ -20,11 +22,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strings"
 	"time"
 )
 
-//
+// Open the input file and convert it to the output format
 func main() {
 
 	flag.Parse()
@@ -63,10 +66,9 @@ func readTransactions(name string) [][]string {
 
 // Works through every line of the input transactions file and converts each to the expected format or discards it.
 // Every line of the input file is parsed even though not all of them produce a corresponding line in the output file.
-// At the end the order of the transactions is reversed (as crypto.com list them in reverse time order).
+// At the end each cryptocurrency's data is gathered together in forward time order (crypto.com lists transactions in reverse time order).
 func convertTransactions(transactions [][]string) [][]string {
 	// The first element must match this exactly otherwise the format may have changed:
-	// expectedFirstRow := []string{"Transaction", "Type", "Currency", "Amount", "USD Equivalent", "Details", "Outstanding Loan", "Date / Time"}
 	expectedFirstRow := []string{"Timestamp (UTC)", "Transaction Description", "Currency", "Amount", "To Currency", "To Amount", "Native Currency", "Native Amount", "Native Amount (in USD)", "Transaction Kind"}
 
 	firstRow := transactions[0]
@@ -75,7 +77,7 @@ func convertTransactions(transactions [][]string) [][]string {
 		log.Fatalf("First row fails to match")
 	}
 
-	output := make([][]string, 0)
+	output := make(map[string][][]string, 0)
 
 	for i, row := range transactions[1:] {
 		csvRowIndex := i + 2
@@ -83,8 +85,8 @@ func convertTransactions(transactions [][]string) [][]string {
 		description := row[1]
 		currency := row[2]
 		amount := row[3]
-		// toCurrency := row[4]
-		// toAmount := row[5]
+		toCurrency := row[4]
+		toAmount := row[5]
 		nativeCurrency := row[6]
 		nativeAmount := row[7]
 		//nativeAmountUSD := row[8]
@@ -93,65 +95,143 @@ func convertTransactions(transactions [][]string) [][]string {
 		ukTime := convertUtcToUKTime(exchangeTime)
 
 		if description == "Sign-up Bonus Unlocked" {
+			// This entry refers to a reward given for using someone's signup code or given when someone else uses your signup code.
 			// Check the required values are as expected
 			if !areRowValuesAcceptable(csvRowIndex, row, currency, "CRO", nativeCurrency, "USD", kind, "referral_gift", "", "") {
 				fmt.Println("Bad value seen")
 				entry := []string{"**BAD DATA**", "crypto.com App", exchangeTime, ukTime, amount, "", nativeAmount, "", "", "", "", "", "", "REWARD **BAD DATA**"}
-				output = append(output, entry)
+				output[currency] = append(output[currency], entry)
 			} else {
 				entry := []string{"", "crypto.com App", exchangeTime, ukTime, amount, "", nativeAmount, "", "", "", "", "", "", "REWARD"}
-				output = append(output, entry)
+				output[currency] = append(output[currency], entry)
 			}
+		} else if description == "Crypto Earn Deposit" {
+			// TODO this just indicates that crypto has been deposited into the Crypto Earn wallet.
+			// No entry is written.
+			// This must be before the code that checks for a suffix of "Deposit", otherwise the code will not interpret the row properly
 		} else if strings.HasSuffix(description, " Deposit") {
 			// TODO check that the deposited token matches 'currency'
-			// This will never result in an entry
-			if nativeCurrency != "GBP" {
-				fmt.Printf("In row %d, expected 'Native Currency' %s but found %s [%s]\n", csvRowIndex, "GBP", nativeCurrency, row)
+			// This will result in a TRANSFER-IN entry
+			fields := strings.Fields(description)
+			depositCurrency := fields[0]
+			if !areRowValuesAcceptable(csvRowIndex, row, currency, depositCurrency, nativeCurrency, "GBP", kind, "crypto_deposit", "", "") {
+				fmt.Println("Bad value seen")
+				entry := []string{"**BAD DATA**", "crypto.com App", exchangeTime, ukTime, amount, "", nativeAmount, "", "", "", "", "", "", "TRANSFER-IN **BAD DATA**"}
+				output[currency] = append(output[currency], entry)
+			} else {
+				entry := []string{"", "crypto.com App", exchangeTime, ukTime, amount, "", nativeAmount, "", "", "", "", "", "", "TRANSFER-IN"}
+				output[currency] = append(output[currency], entry)
 			}
 		} else if strings.Contains(description, " -> ") {
-			// TODO when handling conversions, go from source to target
-			// This should eventually result in an entry for the target and a negative entry for the source (unless it is a currency)
-			fmt.Println("found exchange")
-		} else if description == "CRO Stake" {
-			// TODO When other staking is allowed, check that 'currency' matches the staked currency
-			if nativeCurrency != "GBP" {
-				fmt.Printf("In row %d, expected 'Native Currency' %s but found %s [%s]\n", csvRowIndex, "GBP", nativeCurrency, row)
+			// This entry represents a swap from currency or fiat to a cryptocurrency.
+			// Currently all such entries are from GBP to another currency and represent a simple purchase, so the checks are hard-coded to expect GBP.
+			// This must be after the code that checks for " Crypto Earn Deposit", as this code will not interpret that row properly
+			fields := strings.Fields(description)
+			convertFromCurrency := fields[0]
+			convertToCurrency := fields[2]
+			if !areRowValuesAcceptable(csvRowIndex, row, currency, "GBP", nativeCurrency, convertFromCurrency, kind, "viban_purchase", "", "") || (currency != convertFromCurrency) || (toCurrency != convertToCurrency) {
+				fmt.Println("Bad value seen")
+				entry := []string{"**BAD DATA**", "crypto.com App", exchangeTime, ukTime, toAmount, "", "", "", nativeAmount, "", "", "", "", "BUY **BAD DATA**"}
+				output[currency] = append(output[convertToCurrency], entry)
+			} else {
+				entry := []string{"", "crypto.com App", exchangeTime, ukTime, toAmount, "", "", "", nativeAmount, "", "", "", "", "BUY"}
+				output[convertToCurrency] = append(output[convertToCurrency], entry)
 			}
-		} else if strings.HasSuffix(description, " Stake Rewards") {
-			// TODO: handle staking rewards of cyptocurrencies other than CRO
-			// Check the required values are as expected
+		} else if description == "CRO Stake Rewards" {
+			// crypto.com lists CRO staking rewards in a special way. This entry represnts a staking reward given on the CRO that has been staked for the VISA pre-paid card.
 			// Check the required values are as expected
 			if !areRowValuesAcceptable(csvRowIndex, row, currency, "CRO", nativeCurrency, "GBP", kind, "mco_stake_reward", "", "") {
 				fmt.Println("Bad value seen")
 				entry := []string{"**BAD DATA**", "crypto.com App", exchangeTime, ukTime, amount, "", "", "", nativeAmount, "", "", "", "", "STAKING **BAD DATA**"}
-				output = append(output, entry)
+				output[currency] = append(output[currency], entry)
 			} else {
 				entry := []string{"", "crypto.com App", exchangeTime, ukTime, amount, "", "", "", nativeAmount, "", "", "", "", "STAKING"}
-				output = append(output, entry)
+				output[currency] = append(output[currency], entry)
+			}
+		} else if description == "CRO Stake" {
+			// This entry indicates that CRO has been staked (rather than depostied into the Crypto Earn programme)
+			// No entry is written: this code is here just to check that the data is as expected.
+			// Check the required values are as expected
+			if !areRowValuesAcceptable(csvRowIndex, row, currency, "CRO", nativeCurrency, "GBP", kind, "lockup_lock", "", "") {
+				fmt.Println("Bad value seen")
+				entry := []string{"**BAD DATA**", "crypto.com App", exchangeTime, ukTime, amount, "", "", "", nativeAmount, "", "", "", "", "CRO STAKE **BAD DATA**"}
+				output[currency] = append(output[currency], entry)
 			}
 		} else if description == "Card Cashback" {
+			// This entry represents CRO paid as cashback for purchases made on the VISA card.
 			// Check the required values are as expected
 			if !areRowValuesAcceptable(csvRowIndex, row, currency, "CRO", nativeCurrency, "GBP", kind, "referral_card_cashback", "", "") {
 				fmt.Println("Bad value seen")
 				entry := []string{"***BAD DATA***", "crypto.com App", exchangeTime, ukTime, amount, "", "", "", nativeAmount, "", "", "", "", "CASHBACK **BAD DATA**"}
-				output = append(output, entry)
+				output[currency] = append(output[currency], entry)
 			} else {
 				entry := []string{"", "crypto.com App", exchangeTime, ukTime, amount, "", "", "", nativeAmount, "", "", "", "", "CASHBACK"}
-				output = append(output, entry)
+				output[currency] = append(output[currency], entry)
+			}
+		} else if description == "Crypto Earn" {
+			// This entry represents a currency earned through staking.
+			// TODO: need to find out the currency that has been earned (this is 'currency')
+			entry := []string{"", "crypto.com App", exchangeTime, ukTime, amount, "", "", "", nativeAmount, "", "", "", "", "STAKING"}
+			output[currency] = append(output[currency], entry)
+		} else if description == "Card Cashback Reversal" {
+			// This entry represents a VISA card cashback that has been reversed.
+			// Check the required values are as expected
+			if !areRowValuesAcceptable(csvRowIndex, row, currency, "CRO", nativeCurrency, "GBP", kind, "card_cashback_reverted", "", "") {
+				fmt.Println("Bad value seen")
+				entry := []string{"***BAD DATA***", "crypto.com App", exchangeTime, ukTime, amount, "", "", "", nativeAmount, "", "", "", "", "CASHBACK-REVERSAL **BAD DATA**"}
+				output[currency] = append(output[currency], entry)
+			} else {
+				entry := []string{"", "crypto.com App", exchangeTime, ukTime, amount, "", "", "", nativeAmount, "", "", "", "", "CASHBACK-REVERSAL"}
+				output[currency] = append(output[currency], entry)
+			}
+		} else if strings.HasPrefix(description, "Withdraw ") {
+			// This represents the transfer of a cyrptocurrency to another wallet
+			fields := strings.Fields(description)
+			withdrawCurrency := fields[1]
+
+			// Check the required values are as expected
+			if !areRowValuesAcceptable(csvRowIndex, row, currency, withdrawCurrency, nativeCurrency, "GBP", kind, "crypto_withdrawal", "", "") {
+				fmt.Println("Bad value seen")
+				entry := []string{"***BAD DATA***", "crypto.com App", exchangeTime, ukTime, amount, "", "", "", nativeAmount, "", "", "", "", "TRANSFER-OUT **BAD DATA**"}
+				output[currency] = append(output[currency], entry)
+			} else {
+				entry := []string{"", "crypto.com App", exchangeTime, ukTime, amount, "", "", "", nativeAmount, "", "", "", "", "TRANSFER-OUT"}
+				output[currency] = append(output[currency], entry)
 			}
 		} else {
 			fmt.Printf("UNRECOGNISED <%s>\n", description)
 			entry := []string{"***UNRECOGNISED***", "crypto.com App", exchangeTime, ukTime, amount, "", "", "", nativeAmount, "", "", "", "", "***INVALID***"}
-			output = append(output, entry)
+			output[currency] = append(output[currency], entry)
 		}
 	}
 
-	// hacky code from stackoverflow to reverse the slice
-	for i, j := 0, len(output)-1; i < j; i, j = i+1, j-1 {
-		output[i], output[j] = output[j], output[i]
+	// Find all the currencies in the map
+	currencies := make([]string, 0)
+	for k := range output {
+		currencies = append(currencies, k)
+	}
+	sort.Strings(currencies)
+
+	// Loop through currencies and produce an output that contains all the data categorised by currency
+	finalOutput := make([][]string, 0)
+
+	for _, c := range currencies {
+		data := output[c]
+		// hacky code from stackoverflow to reverse the slice
+		for i, j := 0, len(data)-1; i < j; i, j = i+1, j-1 {
+			data[i], data[j] = data[j], data[i]
+		}
+		// Append the data and a prefix/postfix to the overall output
+		finalOutput = append(finalOutput, []string{"", ""})
+		finalOutput = append(finalOutput, []string{"", ""})
+		finalOutput = append(finalOutput, []string{c, "Data for a fixed currency"})
+		for _, v := range data {
+			finalOutput = append(finalOutput, v)
+		}
+		finalOutput = append(finalOutput, []string{"", ""})
 	}
 
-	return output
+	return finalOutput
 }
 
 // Writes out the accumulated data to the output file in CSV format
