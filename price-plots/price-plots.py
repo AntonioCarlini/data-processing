2afe94cc1cf559244d997b8a5e945b321cd1a11c
Antonio Carlini <antonio@acarlini.com>
2023-04-14 22:34:20 +0100

    price-plots.py: usability improvements
    Handle an HTTP error response (e.g. because the API is rate-limiting us).
    
    Fix argument handling to allow positional arguments to have defaults.
    
    The third argument is supposed to be the number of days over which to plot. make it so!

--- price-plots/price-plots.py
+++ price-plots/price-plots.py
@@ -10,7 +10,7 @@ import requests
 # time and also the ratio of the first crypto against the second over the same time period.
 #
 #  Usage:
-#   python price-plots.py BTC ETH 90 # plots BTC against ETH over the last 90 days
+#   python price-plots.py BTC ETH 180 # plots BTC against ETH over the last 180 days
 
 
 # CoinGecko doesn't use the normal symbols (e.g. BTC) to identify a coin.
@@ -25,15 +25,21 @@ import requests
 #
 # Also enure that symbols are always entirely in lowercase so that matching against the symbol provided on the command line
 # can be simplified.
+#
+# Any error from CoinGecko (i.e. anything other than an HTTP 200 response) causes an immediate exit.
 def build_symbols_to_coingecko_name_map():
+    sym_to_cg_name = {}
     url = "https://api.coingecko.com/api/v3/coins/list"
     r = requests.get(url)
-    identifiers = r.json()
-    sym_to_cg_name = {}
-    for id in identifiers:
-        symbol = id["symbol"].lower() 
-        if not (symbol in sym_to_cg_name) or (len(id["id"]) < len(sym_to_cg_name[symbol])):
-            sym_to_cg_name[symbol] = id["id"]
+    if r.status_code != 200:
+        print("CoinGecko returned an error:", r)
+        exit(1)
+    else:
+        identifiers = r.json()
+        for id in identifiers:
+            symbol = id["symbol"].lower() 
+            if not (symbol in sym_to_cg_name) or (len(id["id"]) < len(sym_to_cg_name[symbol])):
+                sym_to_cg_name[symbol] = id["id"]
     return sym_to_cg_name
 
 #
@@ -47,18 +53,28 @@ def main():
                     prog='plot-prices',
                     description='Plots graphs of two cryptocurrencies and their ratio')
 
-    parser.add_argument('first_crypto', default="BTC")
-    parser.add_argument('second_crypto', default="FLOW")
-    parser.add_argument('days', type=int, default=90)
+    parser.add_argument('first_crypto', nargs='?', default="BTC")
+    parser.add_argument('second_crypto', nargs='?', default="FLOW")
+    parser.add_argument('days', type=int, nargs='?', default=90)
+    parser.add_argument('-d', '--debug', action='store_true')
     args = parser.parse_args()
 
+    # Build map that allows conversion from symbol name (e.g. "BTC") to the coingecko name (in this case, "bitcoin")
     sym_to_name = build_symbols_to_coingecko_name_map()
 
-    print("coin #1: ", args.first_crypto)
-    print("coin #1: ", args.second_crypto)
-    print("days:    ", args.days)
-    print("name #1: ", sym_to_name[args.first_crypto.lower()])
-    print("name #2: ", sym_to_name[args.second_crypto.lower()])
+    # Construct the string that determines how much data to gather e.g. if 100 days is specified on the CLI, build the string "100days"
+    time_span = '{}days'.format(args.days)
+
+    # Dump debug info if requested
+    if args.debug:
+        print("coin #1:    ", args.first_crypto)
+        print("coin #1:    ", args.second_crypto)
+        print("days:       ", args.days)
+        print("cg name #1: ", sym_to_name[args.first_crypto.lower()])
+        print("cg name #2: ", sym_to_name[args.second_crypto.lower()])
+        print("time_span:  ", time_span)
+
+    # Convert CLI coin symbols (converted to lowercase) to CoinGecko names.
     coin_1 = sym_to_name[args.first_crypto.lower()]
     coin_2 = sym_to_name[args.second_crypto.lower()]
 
@@ -66,8 +82,8 @@ def main():
     coinGecko = pycoingecko.CoinGeckoAPI()
 
     # Get historical price data for the two coins
-    coin_1_data = coinGecko.get_coin_market_chart_by_id(coin_1, 'usd', '365days')
-    coin_2_data = coinGecko.get_coin_market_chart_by_id(coin_2, 'usd', '365days')
+    coin_1_data = coinGecko.get_coin_market_chart_by_id(coin_1, 'usd', time_span)
+    coin_2_data = coinGecko.get_coin_market_chart_by_id(coin_2, 'usd', time_span)
 
     # Extract the dates and prices from the data
     coin_1_dates = [data[0] for data in coin_1_data['prices']]
@@ -110,7 +126,7 @@ def main():
     coin_1_to_coin_2_ratio = [coin_1_prices[i]/coin_2_prices[i] for i in range(shorter_list_len)]  
 
     plt.subplot(2, 2, 3)
-    plt.plot(coin_2_dates[-90:], coin_1_to_coin_2_ratio[-90:])
+    plt.plot(coin_2_dates[-args.days:], coin_1_to_coin_2_ratio[-args.days:])
     plt.xlabel('Date')
     plt.ylabel('Price (USD)')
     plt.title('Historical {sym_1}/{sym_2}'.format(sym_1=args.first_crypto, sym_2=args.second_crypto))
